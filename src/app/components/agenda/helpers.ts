24f3470908ca7cc701c76620524ab797bd575051
boernard <boernard@gmail.com>
2020-07-24 08:48:49 +0200

    Styling modal and agenda

--- src/app/components/agenda/helpers.ts
+++ src/app/components/agenda/helpers.ts
@@ -63,3 +63,7 @@ function getHourFromEvents(events, timeProp) {
         .map(splittedTime => splittedTime[0])
         .map(Number)
 }
+
+export function formatDate(date) {
+    return date.replace(/(\d{4})-(\d{2})-(\d{2})/, '$3.$2.$1')
+}
